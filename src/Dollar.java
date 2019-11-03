8578ec80b636267e00de53a2bf607d4d7df1d2c0
taynaracruz <taynaracruz@live.com>
2019-11-03 20:45:18 -0300

    Commit 3

--- src/Dollar.java
+++ src/Dollar.java
@@ -3,12 +3,12 @@ class Dollar extends Money {
     Dollar(int amount) {
        this.amount= amount;
     }
-    Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);	
-    }
+    Money times(int multiplier)  {
+        return new Dollar(amount * multiplier);
+     }	
 
     public boolean equals(Object object)  {
         Dollar dollar = (Dollar) object;
         return amount == dollar.amount;
     }
- }	
+}	
--- src/DollarTest.java
+++ src/DollarTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.*;
 public class DollarTest {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
-    }
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+     }
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
